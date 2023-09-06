8a3f7ca0f352eed43cffbcfefc1aff1adabfe58c
clownzxy <norwenpenas@gmail.com>
2023-09-06 22:35:56 +0800

    Currently working in the picker logic that interacts with School Program and Courses

--- Register.xaml.cs
+++ Register.xaml.cs
@@ -5,5 +5,30 @@ public partial class Register : ContentPage
 	public Register()
 	{
 		InitializeComponent();
-	}
+
+        List<String> pickerList = new List<string>();
+        pickerList.Add("Default");
+        pickerList.Add("Test");
+        pickerList.Add("Test1");
+
+        List<String> course = new List<String>();
+        course.Add("Default");
+
+        picker.ItemsSource = pickerList;
+        picker.SelectedItem = "Default";
+        picker.SelectedIndexChanged += Picker_SelectedIndexChanged;
+    }
+
+    private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        int selectedIndex = picker.SelectedIndex;
+
+
+        if (selectedIndex != -1 )
+        {
+            picker.SelectedItem = picker.Items[selectedIndex];
+        }
+    }
+
 }
